977ec98efbb9fb4b22c7036cf3f782ac2dbfa95d
J-Uptegraph <johnnyup2@gmail.com>
2025-07-15 20:45:55 -0400

    Backup of cori_ws

---
+++ archive/07_15_2025/cori_ws/src/cori_hardware/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'cori_hardware'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/arduino', ['arduino/cori_head_controller/cori_head_controller.ino']),
+        ('share/' + package_name + '/launch', ['launch/hardware_bridge.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='CORI Team',
+    maintainer_email='cori@example.com',
+    description='CORI Hardware Interface - Arduino bridge for physical head movement',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'arduino_bridge = cori_hardware.arduino_bridge:main',
+            'hardware_test = cori_hardware.hardware_test:main',
+            'hardware_integration = cori_hardware.hardware_integration:main',
+            'hardware_bridge = cori_hardware.hardware_bridge:main',
+        ],
+    },
+)
